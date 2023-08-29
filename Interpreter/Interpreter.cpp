2b2f739cafae97c61cfbb014ad556c626c6b6170
Patautista <caleb.kart@gmail.com>
2023-08-28 22:39:14 -0300

    predicate calls working

--- Interpreter/Interpreter.cpp
+++ Interpreter/Interpreter.cpp
@@ -37,7 +37,7 @@ std::map<std::string, Macro> importMacrosFromFile(const std::string& filename, s
 	}
 
 	std::string line;
-	while (std::getline(macroFile, line) && line != "program") {
+	while (std::getline(macroFile, line) ) {
 		// parse macro
 		istringstream iss(line);
 		string macroName;
@@ -45,6 +45,9 @@ std::map<std::string, Macro> importMacrosFromFile(const std::string& filename, s
 		string macroSourceFilename;
 
 		iss >> macroName >> macroType >> macroSourceFilename;
+		if (macroName == "program") {
+			break;
+		}
 
 		ifstream sourceFile(macroSourceFilename);
 		if (!sourceFile.is_open()) {
@@ -65,12 +68,31 @@ std::map<std::string, Macro> importMacrosFromFile(const std::string& filename, s
 	}
 	return macros;
 }
-int executePPL(const std::string& filename, std::map<std::string, int> variables) {
+std::vector<std::string> extractFunctionArguments(string input) {
+	std::istringstream ss(input);
+	ss.ignore(input.find("(")+1, '(');
+	std::vector<std::string> arguments;
 
+	// Read and extract arguments
+	std::string argument;
+	while (ss >> argument ) {
+		arguments.push_back(argument);
+	}
+	// remove parenthesis from last argument
+	if (arguments[arguments.size() - 1].find(")") != string::npos) {
+		arguments[arguments.size() - 1] = arguments[arguments.size() - 1].substr(0, arguments[arguments.size() - 1].find(")"));
+	}
+
+	return arguments;
+}
+int runInstance(const std::string& filename, std::map<std::string, int> variables) {
+
+	// Read macros
 	std::map<std::string, Macro> macros;
 	macros = importMacrosFromFile(filename, macros);
 
 	std::map<std::string, size_t> labels;
+	// Initialize outuput variable
 	variables["y"] = 0;
 
 	std::ifstream inputFile(filename);
@@ -78,6 +100,8 @@ int executePPL(const std::string& filename, std::map<std::string, int> variables
 		std::cerr << "Error: Unable to open the file." << std::endl;
 		return 0;
 	}
+
+	int inputs = 0;
 
 	std::string line;
 	std::vector<std::string> program;
@@ -93,20 +117,31 @@ int executePPL(const std::string& filename, std::map<std::string, int> variables
 				program.push_back(line);
 			}
 		}
-		if (line == "program") {
+		if (line.find("program") != string::npos) {
+			istringstream iss(line);
+			string syntax;
+			iss >> syntax >> syntax;
+			try {
+				inputs = stoi(syntax.substr(1, syntax.size() - 2));
+			}
+			catch (exception e) {}
 			program_start = true;
 		}
 	}
 
 	size_t programCounter = 0;
+
+	// main loop
 	while (programCounter < program.size()) {
-		std::istringstream iss(program[programCounter]);
-		std::string command;
-		// v < - ...
-		// or macros
+		istringstream iss(program[programCounter]);
+		string command;
 		std::vector<std::string> tokens(std::istream_iterator<std::string>{iss},
 			std::istream_iterator<std::string>());
-		if (std::isalpha(tokens[0][0]) && tokens[0] != "if") {
+		if (tokens.size() < 1) {
+			programCounter++;
+			continue;
+		}
+		else if (std::isalpha(tokens[0][0]) && tokens[0] != "if" && tokens[0] != "goto") {
 			// Handle variable operations
 			std::string variable_name = tokens[0];
 			std::string operand;
@@ -161,7 +196,7 @@ int executePPL(const std::string& filename, std::map<std::string, int> variables
 						std::map<std::string, int> variables2;
 						variables2["x1"] = variables[tokens[2]];
 						variables2["x2"] = variables[tokens[4]];
-						result = executePPL(macro.getSource(), variables2);
+						result = runInstance(macro.getSource(), variables2);
 						variables[tokens[0]] = result;
 					}
 					else if (macro.getType() == Macro::Type::FUNCTION) {
@@ -170,16 +205,37 @@ int executePPL(const std::string& filename, std::map<std::string, int> variables
 				else {}
 			}
 		}
-		// goto
+		// if goto or P(...)
 		else if (tokens[0] == "if") {
-			if (variables[tokens[1]] != 0) {
+			int predicateNameEndIndex = tokens[1].find("(");
+			if (predicateNameEndIndex != string::npos) {
+				// parse predicate
+				string predicateName = tokens[1].substr(0, predicateNameEndIndex);
+				// check if predicate is callable
+				if (macros.find(predicateName) != macros.end() && macros[predicateName].getSource() != filename) {
+					vector<string> arguments = {};
+					arguments = extractFunctionArguments(program[programCounter]);
+					map<string, int> variables2;
+					for (int i = 0; i < arguments.size(); i++) {
+						// argument 0 is assigned to x1 and so forth
+						variables2["x" + to_string(i+1)] = variables[arguments[i]];
+					}
+					auto result = runInstance(macros[predicateName].getSource(), variables2);
+					variables["y"] = result;
+				}
+			}
+			else if (variables[tokens[1]] != 0) {
 				std::string label = tokens[5];
+				if (label == "e") {
+					return variables["y"];
+				}
 				if (labels.find(label) != labels.end()) {
 					programCounter = labels[label];
 					continue;
 				}
 			}
 		}
+		// return goto
 		else if (tokens[0] == "goto" && tokens[1] == "e") {
 			return variables["y"];
 		}
@@ -193,7 +249,7 @@ int executePPL(const std::string& filename, std::map<std::string, int> variables
 				std::map<std::string, int> variables2;
 				variables2["x1"] = variables[tokens[2]];
 				variables2["x2"] = variables[tokens[4]];
-				result = executePPL(macro.getSource(), variables2);
+				result = runInstance(macro.getSource(), variables2);
 				variables[tokens[0]] = result;
 			}
 			else if (macro.getType() == Macro::Type::FUNCTION) {
@@ -216,7 +272,7 @@ int main() {
 	std::map<std::string, int> variables;
 	variables["x1"] = 0;
 	variables["x2"] = 0;
-	executePPL(filename, variables);
+	runInstance(filename, variables);
 	system("pause");
 	return 0;
 }
